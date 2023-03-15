4adaa298f20afdcfe59790e2ba758b16da606bc5
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2023-03-15 05:29:26 -0700

    [Bronze V] Title: A+B, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,12 +1,16 @@
-import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int A = scanner.nextInt();
-        int B = scanner.nextInt();
+    public static void main(String[] args) throws IOException { //IOException : 입출력 예외처리
+        /*System.in을 이용해 값을 입력받을 때, 잘못된 값을 입력받은 경우를 대비해 항상 예외 처리를 해줘야 한다.*/
+        /* 0은 아스키코드로 48이다. 즉 입력받은 아스키코드에서 -48을 햐주면 입력받은 숫자가 된다. */
+        int a =  System.in.read() - 48 ; //아스키 코드값이 입력된다. [키보드의 모든 자판에는 아스키코드가 지정]
 
-        System.out.println(A+B);
+        System.in.read(); // System.read.in() 값 입력 후에 '\n'(엔터)가 남아있어서 아래 b입력이 종료된다는 것! 한번더 써줘야한다.
+
+        int b = System.in.read() - 48 ;
+
+        System.out.println(a+b);
 
     }
 }
