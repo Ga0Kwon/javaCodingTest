fbe1e5898c5e600a5583884b86a5b89d59f38b50
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2023-03-19 00:13:16 +0900

    [Bronze V] Title: 아스키 코드, Time: 76 ms, Memory: 11420 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(System.in.read());
+
+        System.out.println(sb);
+    }
+}
