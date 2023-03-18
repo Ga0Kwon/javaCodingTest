fc5440901c2f8267243cd0436d5a8d69d0a4c9c5
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2023-03-19 00:01:06 +0900

    [Bronze V] Title: 문자와 문자열, Time: 80 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -5,10 +5,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String str  = br.readLine();
-        int index = Integer.parseInt(br.readLine())-1; //str의 인덱스는 0부터 시작하므로 -1을 해준다
-        br.close();
 
-        bw.write(str.charAt(index));
+        bw.write(str.charAt(Integer.parseInt(br.readLine())-1));
+        br.close();
         bw.flush();
         bw.close();
     }
