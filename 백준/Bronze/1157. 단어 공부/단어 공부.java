78a38fa9060bc190a7aa3870c632ae22e3120f0e
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2023-03-20 20:33:16 +0900

    [Bronze I] Title: 단어 공부, Time: 116 ms, Memory: 11632 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -3,7 +3,6 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws  Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int[] array = new int[26]; //영문자가 26개
 
@@ -18,12 +17,12 @@ public class Main {
         char result = '?'; // 가장 많았던 문자를 출력하거나 ?을 출력할때 결과 값
 
         for(int i = 0; i < array.length; i++){
-            if(array[i] > count){ //해당 배열에 -97이나 -65을 해서 수를 넣어놨다.[빈도수]
-                /*만약 count보다 크면 계속 해당 값은 count에 넣어서 갱신하는 식으로*/
+            if(array[i] > count){ 
+
                 count = array[i];
-                result = (char)(i+65); //대문자를 출력해야하기 때문에 다시 +65하면 됨
-            }else if(array[i] == count){ //여기서 걸리는 경우는 빈도수가 똑같은 문자가 있다는 것
-                result = '?'; //같은 값이 있으면 result를 '?'로 바꿔줌
+                result = (char)(i+65);
+            }else if(array[i] == count){ 
+                result = '?'; 
             }
         }
 
