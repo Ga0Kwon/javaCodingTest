f7d8f364cbf17e328b55da17fdd20f3dd44a8542
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2023-03-22 21:50:44 +0900

    [Bronze I] Title: 세로읽기, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -7,21 +7,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        char[][] chars = new char[5][15]; //5개 줄 각 줄마다 최대 15글자
+       String[] strs = new String[5];
 
         for(int i = 0; i < 5; i++){
-            String str = br.readLine();
-            for(int j = 0; j < str.length(); j++){
-                chars[i][j] =str.charAt(j);
-            }
+            strs[i] = br.readLine();
         }
         br.close();
 
         for(int i = 0; i < 15; i++){
-            for(int j = 0; j < 5; j++){
-                //null 문자란 문자열의 마지막을 나타내는 문자이다. '\0'(역슬래쉬 0)로 표기
-                if(chars[j][i] == '\0') continue;
-                sb.append(chars[j][i]);
+            for(int j = 0; j < strs.length; j++){
+                if(i < strs[j].length()){
+                    sb.append(strs[j].charAt(i));
+                }
             }
         }
         System.out.println(sb);
