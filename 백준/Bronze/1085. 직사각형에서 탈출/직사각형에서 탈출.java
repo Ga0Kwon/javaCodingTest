0340be2d90de1e16e5f5aed2e5f8203aeef275e0
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2023-03-25 21:59:06 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 76 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+import static java.lang.Math.min;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,18 +17,10 @@ public class Main {
         int h = Integer.parseInt(token.nextToken());
         
         //직사각형의 경계선까지 가는 거리[w, h로 가는 거리가 아니다.]
-        int x_axisMin = findMin(x, w-x);
-        int y_axisMin = findMin(y, h-y);
+        int x_axisMin = min(x, w-x);
+        int y_axisMin = min(y, h-y);
 
-        System.out.println(y_axisMin > x_axisMin ? x_axisMin : y_axisMin);
+        System.out.println(min(x_axisMin, y_axisMin));
     }
-        
-    //최솟값을 찾는 메서드
-    static int findMin(int m, int n){
-        if(m >= n){
-            return n;
-        }else{ //m이 더 작을때
-            return m;
-        }
-    }
+
 }
