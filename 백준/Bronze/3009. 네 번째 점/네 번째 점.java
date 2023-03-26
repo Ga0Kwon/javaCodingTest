796d2f6839c49f6779f92702768acfd46f872dce
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2023-03-26 23:35:49 +0900

    [Bronze III] Title: 네 번째 점, Time: 72 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -8,28 +8,28 @@ public class Main {
 
         StringTokenizer token;
 
-        int x_axis[] = new int[3]; //x 좌표 3개 입력받아서 저장할 배열
-        int y_axis[] = new int[3]; //y 좌표 3개 입력받아서 저장할 배열
+        int x_axis[] = new int[3];
+        int y_axis[] = new int[3];
         
-        for(int i = 0; i < 3; i++) { //3좌표 값만 받으니까 반복문 3번
+        for(int i = 0; i < 3; i++) {
             token = new StringTokenizer(br.readLine());
-            //한줄씩 입력받아서 공백을 기준으로
-            x_axis[i] = Integer.parseInt(token.nextToken()); //앞은 x좌표
-            y_axis[i] = Integer.parseInt(token.nextToken()); //뒤는 y좌표
+
+            x_axis[i] = Integer.parseInt(token.nextToken());
+            y_axis[i] = Integer.parseInt(token.nextToken());
         }
         
-        br.close(); //더이상 입력받을 값 없음
+        br.close();
 
         bw.write(findAxis(x_axis) + " " + findAxis(y_axis));
         bw.flush();
         bw.close();
     }
-    //각각(x와 y)의 좌표값을 찾아주는 메서드
+
     static int findAxis(int[] array){
-        if(array[0] == array[1]){ //만약 x 0인덱스와 1인덱스가 같다면 2개 있다는 뜻임으로
-            return array[2]; //0과 1인덱스가 아닌 2인덱스를 저장
-        }else{ //0과 1인덱스의 x값이 다른데,
-            //만약, x의 0인덱스와 2인덱스가 같으면 1인덱스 저장 그게 아니라면 0인덱스를 저장
+        if(array[0] == array[1]){
+            return array[2];
+        }else{
+
             return (array[0] == array[2]) ? (array[1]) : (array[0]);
         }
     }
