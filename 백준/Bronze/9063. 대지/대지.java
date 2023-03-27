2c15fa57cff93c219e2128d16ce2453c3ace837a
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2023-03-27 21:12:06 +0900

    [Bronze III] Title: 대지, Time: 232 ms, Memory: 25684 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.java
+++ 백준/Bronze/9063. 대지/대지.java
@@ -1,46 +1,46 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer token;
 
-        int[][] axis = new int[n][2];
+        int x_axis[] = new int[n]; //x좌표 배열 따로
+        int y_axis[] = new int[n]; //y좌표 배열 따로
 
-        for(int i = 0; i < n; i++){
-            token = new StringTokenizer(br.readLine());
-            for(int j = 0; j < 2; j++){
-                axis[i][j] = Integer.parseInt(token.nextToken());
+        int x_min = Integer.MAX_VALUE;
+        int x_max = Integer.MIN_VALUE;
+        int y_min = Integer.MAX_VALUE;
+        int y_max = Integer.MIN_VALUE;
+
+        for(int i  = 0; i < n; i++){
+            //어차피 token for문 안에서만 쓰니까
+            StringTokenizer token = new StringTokenizer(br.readLine());
+            x_axis[i] = Integer.parseInt(token.nextToken());
+            y_axis[i] = Integer.parseInt(token.nextToken());
+
+
+            //for문을 최소화로 하기 위해 if문으로 해서 각 x, y의 최솟값을 구했다.
+            if(y_axis[i] >  y_max){
+                y_max = y_axis[i];
+            }
+
+            if(y_axis[i] < y_min){
+                y_min = y_axis[i];
+            }
+
+            if(x_axis[i] >  x_max){
+                x_max = x_axis[i];
+            }
+
+            if(x_axis[i] < x_min){
+                x_min = x_axis[i];
             }
         }
         br.close();
 
-        int x_min = axis[0][0];
-        int x_max = axis[0][0];
-        int y_min = axis[0][1];
-        int y_max = axis[0][1];
-
-        for(int i = 0; i < n; i++) {
-            if (x_max < axis[i][0]) {
-                x_max = axis[i][0];
-            }
-
-            if (x_min > axis[i][0]) {
-                x_min = axis[i][0];
-            }
-
-            if (y_max < axis[i][1]) {
-                y_max = axis[i][1];
-            }
-
-            if (y_min > axis[i][1]) {
-                y_min = axis[i][1];
-            }
-        }
-
         System.out.println((x_max-x_min)*(y_max-y_min));
-
     }
 }
