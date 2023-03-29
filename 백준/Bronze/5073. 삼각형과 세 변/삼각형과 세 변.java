77be13f9252c480626c2dd8522e71d7771632dfb
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2023-03-29 21:13:35 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 76 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -9,22 +9,21 @@ public class Main {
         while(true) {
             StringTokenizer token = new StringTokenizer(br.readLine());
             
-            //배열이 아닌 int형 변수 선언
-            int a = Integer.parseInt(token.nextToken());
-            int b = Integer.parseInt(token.nextToken());
-            int c = Integer.parseInt(token.nextToken());
+            //배열로 저장
+           int [] array = {Integer.parseInt(token.nextToken()),Integer.parseInt(token.nextToken()),Integer.parseInt(token.nextToken())};
 
-            if(a == 0&& b == 0 && c == 0){
+
+            if(array[0] == 0&& array[1] == 0 && array[2] == 0){
                 break;
             }
 
-            if((a >= b+ c) || (b >= a+ c) || (c >= a+ b)){ //삼각형에 부합하지 않을 경우
+            if((array[0] >= array[1]+ array[2]) || (array[1] >= array[0]+ array[2]) || (array[2] >= array[0]+ array[1])){ //삼각형에 부합하지 않을 경우
                 bw.write("Invalid\n");
-            }else if(a ==b && b == c){ //세변이 같을 경우
+            }else if(array[0] ==array[1] && array[1] == array[2]){ //세변이 같을 경우
                 bw.write("Equilateral\n");
-            }else if((a == b) || (a == c) || (b == c)){ //두변이 같을 경우
+            }else if((array[0] == array[1]) || (array[0] == array[2]) || (array[1] == array[2])){ //두변이 같을 경우
                 bw.write("Isosceles\n");
-            }else if((a != b) && (b != c) && (c != a)){ //세변이 전부 다를 경우
+            }else if((array[0] != array[1]) && (array[1] != array[2]) && (array[2] != array[0])){ //세변이 전부 다를 경우
                 bw.write("Scalene\n");
             }
         }
