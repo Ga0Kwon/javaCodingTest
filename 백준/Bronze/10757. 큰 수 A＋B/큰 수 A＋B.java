a7c0693abf5fb482d45daaaec42ce63e29bb82c5
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2023-04-03 16:23:58 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 184 ms, Memory: 15088 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+
+        //(0 < A,B < 10^10000) : 자료형 BigDecimal 을 사용
+
+        //BigDecimal는 숫자를 정밀하게 저장하표현할 수 있는 객체
+        //단점 :  느린 속도 & 복잡한 사용법
+        System.out.println((new BigDecimal(st.nextToken()).add(new BigDecimal(st.nextToken())))); //A+B식으로 하지 못한다. add() 메서드를 사용해야함
+    }
+}
