0ba9e8869641db8f360fef78a6c9c83d179b9135
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2023-04-11 22:41:30 +0900

    [Bronze II] Title: 블랙잭, Time: 84 ms, Memory: 11656 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -18,23 +18,29 @@ public class Main {
         for(int i = 0; i < N; i++) {
             numberArray[i] = Integer.parseInt(st.nextToken());
         }
+        bw.write( blackjack(numberArray, N, M) + "\n");
+        bw.flush();
+        bw.close();
 
+    }
+
+    public static int blackjack(int[] arrays, int N, int M){
         int sum = 0; //3수의 합을 구하는 변수
         int temp = 0; //근사치를 저장하여 저장하는 변수 (이전 for문의 3개의 합을 더해서 계속 비교해준다.
 
         for(int i = 0; i < N-2; i++) { //숫자가 겹치면 안되기 때문에 i는 0부터 시작하고 N-2까지
             for(int j = i+1; j < N-1; j++) { // 다음 j는 i+1 부터 시작하고 N-1까지
                 for(int k = j+1; k < N; k++) { //다음 k는 j+1부터 시작 N까지 반복
-                    sum = numberArray[i] + numberArray[j] + numberArray[k]; //3수를 합쳐서
+                    sum = arrays[i] + arrays[j] + arrays[k]; //3수를 합쳐서
+                    if(sum == M){ //수가 이미 같으면 바로 return 한다.
+                        return sum;
+                    }
                     if(sum > temp && sum <= M){ //이전 근사치보다 큰데 M보다 작거나 같다는 것은 이전 근사치보다 M에 가깝다는 뜻
                         temp = sum;
                     }
                 }
             }
         }
-        bw.write(temp + "\n");
-        bw.flush();
-        bw.close();
-
+        return temp; // 완전히 같은 수가 없을 때는 temp(가장 근사치)반환
     }
 }
