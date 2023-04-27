96a677bae60682c14c51a37483fa22be6006c70c
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2023-04-27 09:57:36 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 660 ms, Memory: 58212 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,3 +1,4 @@
+import javafx.scene.layout.CornerRadii;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -8,33 +9,48 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st ;
 
+        ArrayList<Coordinate> coorList = new ArrayList<Coordinate>(); 
+
         int N = Integer.parseInt(br.readLine());
-        int[][] arrays = new int[N][2];
 
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            arrays[i][0] = Integer.parseInt(st.nextToken());
-            arrays[i][1] = Integer.parseInt(st.nextToken());
+            coorList.add(new Coordinate(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
         br.close();
 
-        Arrays.sort(arrays, (num1, num2) -> {
-            return num1[1]!=num2[1] ? num1[1]-num2[1] : num1[0]-num2[0];
-        });
+        Collections.sort(coorList); 
 
         for(int i = 0; i < N; i++){
-            bw.write(arrays[i][0] + " " + arrays[i][1] +"\n");
+            bw.write(coorList.get(i).toString());
         }
 
         bw.flush();
         bw.close();
+    }
+}
 
+class Coordinate implements Comparable<Coordinate>{
+    int x ; //x 좌표
+    int y; //y 좌표
 
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Coordinate o) { 
+        if (this.y != o.y) return this.y - o.y; 
+        else return this.x - o.x; 
+    }
+
+    public String toString() {
+        return x + " " + y + "\n";
     }
 }
