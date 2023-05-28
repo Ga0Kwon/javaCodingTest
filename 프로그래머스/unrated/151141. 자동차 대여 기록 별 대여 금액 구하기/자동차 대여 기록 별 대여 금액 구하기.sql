63141099eb4c2745861a094072c1a222b2d93b50
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2023-05-28 17:21:47 +0900

    [unrated] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,11 +1,29 @@
-SELECT HISTORY_ID,
-    round(DAILY_FEE * (DATEDIFF(H.END_DATE,H.START_DATE)+1)
-    * (1 - P.DISCOUNT_RATE/ 100)) FEE
+SELECT H.HISTORY_ID,
+    -- 지불해야하는 비율까지 구했다면 일일 대여 요금 x (끝난 날짜 - 시작 날짜)+1 = 이용기간 => 대여 금액 
+    -- 단 소수점은 다 버린다.
+    round(DAILY_FEE * (DATEDIFF(H.END_DATE,H.START_DATE)+1) * 
+          (1 - ( -- 1 - (구한 비율을 100으로 나눈 값)을 하면 비용을 지불해야하는 비율이다.
+          CASE WHEN DATEDIFF(H.END_DATE,H.START_DATE)+1 >= 90  -- 90일 이상일 경우
+                THEN (SELECT DISCOUNT_RATE  -- CAR_RENTAL_COMPANY_DISCOUNT_PLAN테이블에서 할인비율
+                        FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN SP
+                      -- CAR_RENTAL_COMPANY_CAR의 차종류와 CAR_RENTAL_COMPANY_DISCOUNT_PLAN의 차 종류가 같고
+                     WHERE C.CAR_TYPE = SP.CAR_TYPE 
+                        and DURATION_TYPE = '90일 이상')/100 -- 대여 기간이 90일 이상인 것
+                    -- 의 비율을 100으로 나눈 후 
+              -- CASE ~ WHEN ~ END의 내용은 위와 같은 내용이다.
+               WHEN DATEDIFF(H.END_DATE,H.START_DATE)+1 >= 30 
+                THEN (SELECT DISCOUNT_RATE 
+                        FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN SP
+                     WHERE C.CAR_TYPE = SP.CAR_TYPE and DURATION_TYPE = '30일 이상')/100
+               WHEN DATEDIFF(H.END_DATE,H.START_DATE)+1 >= 7 
+                THEN (SELECT DISCOUNT_RATE 
+                        FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN SP
+                     WHERE C.CAR_TYPE = SP.CAR_TYPE and  DURATION_TYPE = '7일 이상')/100
+               ELSE 0 -- 90, 30, 7일 이상이 아닐 경우는 1 - 0 = 할인이 없다는 것
+               END ))) FEE
     from CAR_RENTAL_COMPANY_CAR as C
         join CAR_RENTAL_COMPANY_RENTAL_HISTORY as H
-        on C.CAR_ID = H.CAR_ID
-        join CAR_RENTAL_COMPANY_DISCOUNT_PLAN as P
-        on C.CAR_TYPE = P.CAR_TYPE
+            on C.CAR_ID = H.CAR_ID -- CAR_RENTAL_COMPANY_RENTAL_HISTORY의 테이블의 차 ID와 
 WHERE C.car_type = "트럭"
-GROUP BY HISTORY_ID
-ORDER BY FEE DESC , HISTORY_ID DESC
+GROUP BY H.HISTORY_ID
+ORDER BY FEE DESC , H.HISTORY_ID DESC
