869ad9e02f6da0099ef474924be138a056463a83
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2023-08-13 14:01:34 +0900

    [Silver IV] Title: 덱 2, Time: 1220 ms, Memory: 267060 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.java
+++ 백준/Silver/28279. 덱 2/덱 2.java
@@ -16,25 +16,33 @@ public class Main {
 
         while(N --> 0) {
             st = new StringTokenizer(br.readLine());
-            String order = st.nextToken();
+            int order = Integer.parseInt(st.nextToken());
 
-            //명령어 1 X : 덱의 앞에 넣는다
-            if (order.equals("1")) {
-                frontPush(Integer.parseInt(st.nextToken()));
-            } else if (order.equals("2")) {//명령어 2 X : 덱의 뒤에 넣는다
-                backPush(Integer.parseInt(st.nextToken()));
-            } else if (order.equals("3")) {//명령어 3 : 덱의 맨 앞의 정수를 빼서 출력한다.(없다면 -1)
-                frontPop();
-            } else if (order.equals("4")) {//명령어 4 : 덱의 맨 뒤의 정수를 빼서 출력한다.(없다면 -1)
-                backPop();
-            } else if (order.equals("5")) {//명령어 5 : 덱에 들어있는 정수의 개수 출력
-                dequeSize();
-            } else if (order.equals("6")) {//명령어 6 : 덱이 비어있으면 1 출력 비어있지 않으면 0
-                dequeEmpty();
-            } else if (order.equals("7")) {//명령어 7 : 덱에 정수가 있다면 맨 앞의 정수를 출력(없다면 -1)
-                getFront();
-            } else if (order.equals("8")) {//명령어 8 : 덱에 정수가 있다면 맨 뒤의 정수를 출력(없다면 -1)
-                getBack();
+            switch (order){
+                case 1 : //명령어 1 X : 덱의 앞에 넣는다
+                    frontPush(Integer.parseInt(st.nextToken()));
+                    break;
+                case 2 : //명령어 2 X : 덱의 뒤에 넣는다
+                    backPush(Integer.parseInt(st.nextToken()));
+                    break;
+                case 3 : //명령어 3 : 덱의 맨 앞의 정수를 빼서 출력한다.(없다면 -1)
+                    frontPop();
+                    break;
+                case 4 : //명령어 4 : 덱의 맨 뒤의 정수를 빼서 출력한다.(없다면 -1)
+                    backPop();
+                    break;
+                case 5 : //명령어 5 : 덱에 들어있는 정수의 개수 출력
+                    dequeSize();
+                    break;
+                case 6 : //명령어 6 : 덱이 비어있으면 1 출력 비어있지 않으면 0
+                    dequeEmpty();
+                    break;
+                case 7 : //명령어 7 : 덱에 정수가 있다면 맨 앞의 정수를 출력(없다면 -1)
+                    getFront();
+                    break;
+                case 8 : //명령어 8 : 덱에 정수가 있다면 맨 뒤의 정수를 출력(없다면 -1)
+                    getBack();
+                    break;
             }
         }
         br.close();
